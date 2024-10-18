65f38f7657adfe3d58d3587e1c9df7bb34a9608c
xaqan05 <xaqan2005@gmail.com>
2024-10-18 16:39:05 +0400

    version1

---
+++ Task(18.10)/Program.cs
@@ -0,0 +1,201 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Task_18._10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            bool condition = false;
+
+            string input;
+            string input2;
+            string name;
+            int charger=40;
+            int bulletNum;
+            bool fireMod = false;
+            int addBullet;
+
+            Weapon weapon = null;
+
+            do
+            {
+                Console.WriteLine("0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Silah daxil etmek ucun");
+                Console.WriteLine("2 - Shoot metodu üçün");
+                Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("4 - Reload metodu üçün");
+                Console.WriteLine("5 - ChangeFireMode metodu üçün");
+                Console.WriteLine("6 - Gulle elave etmek ucun");
+                Console.WriteLine("7 - Proqramdan dayandırmaq üçün");
+
+                Console.WriteLine(" ");
+
+                Console.WriteLine("Zehmet olmasa yuxarida verilenlere esasen qiymet daxil edin:");
+                input = Console.ReadLine();
+
+
+                switch (input)
+                {
+                    case "1":
+
+                        Console.Clear();
+                        Console.Write("Zehmet olmasa silah adini daxil edin:");
+                        name = Console.ReadLine();
+
+                        Console.WriteLine(" ");
+                        bool condition1;
+                        do
+                        {
+
+                            Console.WriteLine("Zehmet olmasa daraqdaki tutumun daxil edin (Daraq tutumu 0 ve 40 arasi olmalidir.) :");
+                            condition1 = int.TryParse(Console.ReadLine(), out charger);
+                            if(charger > 40)
+                            {
+                                condition1 = false;
+                                Console.WriteLine("Duzgun qiymet daxil etmediniz.");
+                                Console.WriteLine(" ");
+                                    
+                            }
+                            
+                        }
+                        while (!condition1);
+
+                        Console.WriteLine(" ");
+
+                        do
+                        {
+                            Console.WriteLine("Zehmet olmasa gulle sayini daxil edin:");
+                            condition1 = int.TryParse(Console.ReadLine(), out bulletNum);
+
+                        } while (!condition1);
+
+                        Console.WriteLine(" ");
+
+                        do
+                        {
+                            Console.WriteLine("Zehmet olmasa silah modunu secin:");
+                            Console.WriteLine("Avomatik mod ucun 1, tekli mod ucun 0 daxil edin:");
+                            input2 = Console.ReadLine();
+
+                            switch (input2)
+                            {
+                                case "1":
+                                    fireMod = true;
+                                    break;
+                                case "0":
+                                    fireMod = false;
+                                    break;
+                            }
+                        } while (!condition1);
+
+                        weapon = new Weapon(name, charger, bulletNum, fireMod);
+                        Console.WriteLine("Silah ugurlu bir sekilde yaradildi");
+
+                        break;
+
+                    case "0":
+
+                        Console.Clear();
+                        Console.WriteLine(" ");
+                        if(weapon != null)
+                        {
+                            if (fireMod)
+                            {
+                            Console.WriteLine($"Silahin adi: {weapon.weaponName}, Daraq tutumu: {weapon.Charger}, Daraqda olan gulle sayi: {weapon.chargeBulletNum}, Gulle sayi: {weapon.BulletNum} , Silah modu: Avtomatik");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Silahin adi: {weapon.weaponName}, Daraq tutumu: {weapon.Charger}, Daraqda olan gulle sayi: {weapon.chargeBulletNum}, Gulle sayi: {weapon.BulletNum} , Silah modu: Tekli");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zehmet olmasa ilk once silah yaradin.");
+                        }
+                        break;
+                    case "2":
+                        Console.Clear();
+
+                        if (weapon != null)
+                        {
+                            weapon.Shoot();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zehmet olmasa ilk once silah yaradin.");
+                        }
+                        break;
+                    case "3":
+                        Console.Clear();
+
+                        if (weapon != null)
+                        {
+                            weapon.GetRemainBulletCount();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zehmet olmasa ilk once silah yaradin.");
+                        }
+                        break;
+                    case "4":
+                        Console.Clear();
+
+                        if (weapon != null)
+                        {
+                            weapon.Reload();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zehmet olmasa ilk once silah yaradin.");
+                        }
+                        break;
+                    case "5":
+                        Console.Clear();
+                        if (weapon != null)
+                        {
+                            weapon.ChangeFireMode();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Zehmet olmasa ilk once silah yaradin.");
+                        }
+                        break;
+                        case "6":
+                            bool condition2;
+                        do
+                        {
+                            Console.WriteLine("Zehmet olmasa elave etmek istediyiniz gulle sayini daxil edin:");
+                            condition2 = int.TryParse(Console.ReadLine(), out addBullet);
+
+                            if(weapon != null)
+                            {
+                                weapon.addBullet(addBullet);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Zehmet olmasa ilk once silah yaradin");
+                            }
+
+                        } while (!condition2);
+                        break;
+                    case "7":
+                        Console.WriteLine("Proqramdan cixilir...");
+                        condition = true;
+                        break;
+                    default:
+                        Console.WriteLine("Zehmet olmasa duzgun secim edin.");
+                        break;
+                }
+
+            } while (!condition);
+
+
+
+        }
+    }
+}
---
+++ Task(18.10)/Weapon.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_18._10_
+{
+    public class Weapon
+    {
+        public string weaponName;
+
+        private int _charger;
+        private int _bulletNum;
+
+        public bool changeMod;
+
+        public int chargeBulletNum = 0;
+        public int remainingBullet;
+
+        public int Charger
+        {
+            get
+            {
+                return _charger;
+            }
+            set
+            {
+                if (value <= 41 && value > 0)
+                {
+                    _charger = value;
+                    chargeBulletNum = value;
+                }
+            }
+        }
+
+        public int BulletNum
+        {
+            get
+            {
+                return _bulletNum;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletNum = value;
+                }
+            }
+        }
+
+        public Weapon(string name, int charger, int bulletNum, bool weaponMode)
+        {
+            weaponName = name;
+            _charger = charger;
+            _bulletNum = bulletNum;
+            changeMod = weaponMode;
+        }
+
+        public void Shoot()
+        {
+            if (chargeBulletNum == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Silahi ilk once reload edin.");
+            }
+            else if (changeMod)
+            {
+                chargeBulletNum = 0;
+                remainingBullet = chargeBulletNum;
+                Console.WriteLine("Avtomatik modda oldugunuz ucun gulleniz bitdi.");
+            }
+            else
+            {
+                chargeBulletNum--;
+                remainingBullet = Charger - chargeBulletNum;
+                Console.WriteLine($"Tekli modda oldugu ucun 1 gulle istifade etdiniz.Daragda qalan gulle sayi: {chargeBulletNum}. {BulletNum} gulleniz movcuddur.");
+            }
+        }
+        public void GetRemainBulletCount()
+        {
+            int requiredBullet = Charger - chargeBulletNum;
+
+            Console.WriteLine($"Daragda qalan gulle sayi: {chargeBulletNum}, Dolmasi ucun lazim olan gulle sayi: {requiredBullet} ");
+        }
+
+        public void Reload()
+        {
+            if(BulletNum <= Charger)
+            {
+                Console.WriteLine("Reload etmek ucun yeterli gulleniz yoxdur");
+            }
+            if (BulletNum >= Charger)
+            {
+                chargeBulletNum +=Charger;
+                int required = BulletNum - Charger;
+                BulletNum = required;
+                Console.WriteLine("Silah reload olundu.");
+            }
+            Console.WriteLine(" ");
+
+        }
+
+        public void addBullet(int bullet)
+        {
+            BulletNum += bullet;
+            Console.WriteLine($"{bullet} gulle elave olundu.");
+        }
+        public void ChangeFireMode()
+        {
+            changeMod = !changeMod;
+
+            if (changeMod)
+            {
+                Console.WriteLine("Silah avtomatik moda kecid etdi");
+            }
+            else
+            {
+                Console.WriteLine("Silah tekli moda kecid etdi");
+            }
+        }
+
+        public void ShowInfo()
+        {
+            if (changeMod)
+            {
+                Console.WriteLine($"Silahin adi: {weaponName}, Daraq tutumu: {Charger}, Daraqda olan gulle sayi: {chargeBulletNum}, Gulle sayi: {BulletNum} , Silah modu: Avtomatik");
+            }
+            else
+            {
+                Console.WriteLine($"Silahin adi: {weaponName}, Daraq tutumu: {Charger}, Daraqda olan gulle sayi: {chargeBulletNum}, Gulle sayi: {BulletNum} , Silah modu: Tekli");
+            }
+        }
+    }
+}
